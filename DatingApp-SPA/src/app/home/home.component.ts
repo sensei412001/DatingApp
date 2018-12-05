b21985097c802233a738f43151dfd8aca0569072
Malate <emil.malate@lrmc.com.ph>
2018-12-05 13:26:51 +0800

    End of Section 5

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  registerMode = true;
+  registerMode = false;
 
   constructor(private http: HttpClient) { }
 
